0890fe178a40009199159d9f85db5b832a465c9e
Daniel Tucker <TUCKERD1@ETSU.EDU>
2022-11-17 19:19:23 -0500

    Add project files.

---
+++ Lab6 JSON/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_JSON.Models
+{
+    public class Book
+    {
+       public Items[] Items { get; set; }
+
+       public Book(Items[] items)
+        {
+            Items = items;  
+        }
+        public override string ToString()
+        {
+            string bookString = "";
+            bookString += $"ID: {string Id}
+            bookString += $"SelfLink:
+            bookString += $"VolumeInfo:
+            bookString += $"Title:
+            bookString += $"Authors:
+            bookString += $"Description:
+
+        }
+    }
+}
---
+++ Lab6 JSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lab6_JSON.Models;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+// points to data 
+var filePath= root + $"{Path.DirectorySeparatorChar}Data{Path.DirectorySeparatorChar}Guns and Control.json";
+var dataPath= root + $"{Path.DirectorySeparatorChar}Data";
+
+var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+foreach (string filename in Directory.GetFiles(dataPath))
+{
+    string jsonString = string.Empty;
+    using (StreamReader sr = new StreamReader(filename))
+    {
+        jsonString = sr.ReadToEnd();
+    }
+
+    Console.WriteLine(jsonString);
+
+    var books = JsonSerializer.Deserialize<Book>(jsonString, options);
+
+    Console.WriteLine(books);
+}
